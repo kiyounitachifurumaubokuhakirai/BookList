c53d53bbc9009db1b6ac3437fd2f4dd16aa9dacb
M <>
2020-02-14 15:51:35 +0900

    共通の定義ファイルを作成

---
+++ htdocs/common/define.php
@@ -0,0 +1,23 @@
+<?php
+
+  //Database接続関連
+  define('DB_HOST', 'localhost');
+  define('DB_NAME', 'booklist');
+  define('DB_USER', 'root');
+  //define('DB_PASS', '');   //for windows
+  define('DB_PASS', 'root');   //for mac
+
+  //エラーメッセージ関連
+  define('MSG_ERR', '%sに誤りがあります。');
+  define('MSG_EXCEPTION', '申し訳ございません、エラーが発生しました。');
+
+  //サニタイズ
+  function sanitize($before){
+    $after=[];
+    foreach($before as $key => $value){
+      $after[$key]=htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+    return $after;
+  }
+
+?>
