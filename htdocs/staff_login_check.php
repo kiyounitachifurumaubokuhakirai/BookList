331c3fa321dd6b23dbb2582738f79f1561c9b3c8
M <>
2020-02-27 11:44:12 +0900

    ログインの項目が正しく入力されているかチェックするファイルを作成

---
+++ htdocs/staff_login_check.php
@@ -0,0 +1,56 @@
+<?php
+  session_start();
+  session_regenerate_id(TRUE);
+
+  require_once(dirname(__FILE__).'/common/define.php');
+  require_once(dirname(__FILE__).'/common/sql_staff.php');
+
+  unset($_SESSION['err']);
+  unset($_SESSION['login']);
+
+  $post = [];
+  $post = sanitize($_POST);
+
+
+  foreach($post as $key => $value){
+    $_SESSION['login'][$key] = $post[$key];
+  }
+
+  $validity = TRUE;
+
+  //ユーザ名
+  if(!$_SESSION['login']['user']){
+    $validity = FALSE;
+    $_SESSION['err']['login']['user'] = 'ユーザー名が入力されていません';
+  }
+  //パスワード
+  if(!$_SESSION['login']['pass']){
+    $validity = FALSE;
+    $_SESSION['err']['login']['pass'] = 'パスワードが入力されていません';
+  }
+
+  if($validity == FALSE){
+    header('Location: staff_login.php');
+    exit();
+  }
+
+  try{
+    $staff = new StaffModel;
+    if($staff->LoginCheck($_SESSION["login"]['user'], $_SESSION["login"]['pass'])){
+      header('Location: ./after_login/message.php');
+    }
+    else{
+      $_SESSION['err']['login']['incorrect'] = 'ユーザー名とパスワードが一致しません';
+      header('Location: staff_login.php');
+    }
+  }
+  catch(Exception $e){
+    var_dump($e);
+    header('Location: ./index.php');
+    exit();
+  }
+
+  unset($_SESSION["login"]['pass']);
+  $staff = NULL;
+
+?>
