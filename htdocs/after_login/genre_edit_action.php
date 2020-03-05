c936e0f8986669047a9286a002fe391754c54307
M <>
2020-03-05 11:31:50 +0900

    ジャンルに関しての処理を実行するファイルを作成

---
+++ htdocs/after_login/genre_edit_action.php
@@ -0,0 +1,69 @@
+<?php
+  if(!isset($_SESSION)) session_start();
+  session_regenerate_id(TRUE);
+
+  require_once('../common/sql_genre.php');
+
+  try{
+    $genre = new genreModel;
+    $genre->addGenre($_SESSION['genre']['name']['genreName']);
+  }
+
+  catch(Exception $e){
+    var_dump($e);
+    header('Location: ../index.php');
+    exit();
+  }
+
+  $genre = NULL;
+  unset($_SESSION['genre']);
+
+?>
+
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>書籍修正</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+</head>
+<body>
+<div class="container mt-5">
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a href="home.php" class="nav-link">HOME</a>
+      </li>
+      <li class="nav-item">
+        <a href="search_books.php" class="nav-link active">書籍検索</a>
+      </li>
+      <li class="nav-item">
+        <a href="request.php" class="nav-link">書籍リクエスト</a>
+      </li>
+      <li class="nav-item">
+        <a href="staff_login.php" class="nav-link">スタッフ管理</a>
+      </li>
+    </ul>
+  </div>
+  
+  <div class="container">
+    <div class="my-3">
+
+        <h2>登録完了</h2>
+
+      <div class="my-2">
+        <p>登録しました</p>
+      </div>
+
+    </div>
+  </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
