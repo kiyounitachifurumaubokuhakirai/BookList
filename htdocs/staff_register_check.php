a4c7330ba1b29baaf9b4dae3919fbcc2f567445e
M <>
2020-03-18 11:11:38 +0900

    NAVの変更

--- htdocs/staff_register_check.php
+++ htdocs/staff_register_check.php
@@ -79,21 +79,44 @@
     
 </head>
 <body>
-  <div class="container mt-5">
+<div class="container mt-5">
     <ul class="nav nav-tabs">
       <li class="nav-item">
         <a href="index.php" class="nav-link">HOME</a>
       </li>
-      <li class="nav-item">
-        <a href="" class="nav-link active">スタッフ新規登録</a>
-      </li>
-      <li class="nav-item">
-        <a href="staff_login.php" class="nav-link">ログイン</a>
-      </li>
-      <li class="nav-item">
-        <a href="" class="nav-link">未読リクエスト <span class="badge badge-secondary">New</span></a>
-      </li>
-    </ul>
+      <?php if(isset($_SESSION["login"]['is_login']) && $_SESSION["login"]['is_login']==TRUE):?>
+        <li class="nav-item">
+          <a href="./after_login/message.php" class="nav-link">未読リクエスト
+            <?PHP if(isset($_SESSION['login']['is_all_completed']) && !$_SESSION['login']['is_all_completed']):?> <span class="badge badge-secondary">New</span><?PHP endif?>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="./after_login/book_register.php" class="nav-link">書籍登録</a>
+        </li>
+        <li class="nav-item">
+          <a href="./after_login/search.php" class="nav-link">書籍修正</a>
+        </li>
+        <li class="nav-item">
+          <a href="./after_login/genre.php" class="nav-link">ジャンル登録・修正・削除</a>
+        </li>
+        <li class="nav-item">
+          <a href="staff_register.php" class="nav-link active">スタッフ新規登録</a>
+        </li>
+        <li class="nav-item">
+          <a href="./after_login/staff_edit_delete.php" class="nav-link">スタッフ編集・削除</a>
+        </li>
+        <li class="nav-item">
+          <a href="./after_login/logout.php" class="nav-link">ログアウト</a>
+        </li>
+      <?php else:?>
+        <li class="nav-item">
+          <a href="staff_register.php" class="nav-link active">スタッフ新規登録</a>
+        </li>
+        <li class="nav-item">
+          <a href="staff_login.php" class="nav-link">ログイン</a>
+        </li>
+      <?php endif?>
+  </ul>
   </div>
 
   <div class="container my-3">
@@ -118,10 +141,10 @@
 
       <div class="form-group row">
         <div class="form-check form-check-inline">
-          <button type="submit" class="btn btn-secondary" onclick="location.href='staff_register.php'">戻る</button>
+          <button type="reset" class="btn btn-secondary" onclick="location.href='staff_register.php'">戻る</button>
         </div>
         <div class="form-check form-check-inline">
-          <button type="submit" class="btn btn-primary" onclick="location.href='staff_register_action.php'">登録</button>
+          <button type="submit" class="btn btn-primary">登録</button>
         </div>
       </div>
 
