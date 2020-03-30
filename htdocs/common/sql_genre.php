f36c189dbbe1764afcb031f00ed79979bb2d57a6
M <>
2020-03-30 14:43:45 +0900

    1. 曖昧検索において、$idがない場合にも対応 2. 重複確認の処理を追加 3. 編集処理を追加

--- htdocs/common/sql_genre.php
+++ htdocs/common/sql_genre.php
@@ -51,12 +51,16 @@
     //ジャンル名検索（曖昧検索） $idによるgenreは除く
     public function searchLikeGenre($id, $genre) : array {
       $likeGenre = [];
-
-      $sql = "SELECT * FROM genre_list WHERE is_deleted=0 and id!=? and genre like ?";
-      $stmt = $this->dbh->prepare($sql);
       $data = [];
-      $data[] = $id;
+
+      if($id){
+        $data[] = $id;
+        $sql = "SELECT * FROM genre_list WHERE is_deleted=0 and id!=? and genre like ?";
+      } 
+      else  $sql = "SELECT * FROM genre_list WHERE is_deleted=0 and genre like ?";
+      
       $data[] = "%".$genre."%";
+      $stmt = $this->dbh->prepare($sql);
       $stmt->execute($data);
 
       while(TRUE){
@@ -71,7 +75,7 @@
 
 
 
-      //ジャンル名検索（id検索）
+    //ジャンル名検索（id検索）
     public function searchGenreFromID($id)  {
       $sql = "SELECT genre FROM genre_list WHERE id=?";
       $stmt = $this->dbh->prepare($sql);
@@ -83,14 +87,55 @@
     }
 
 
+
+    //ジャンル名検索 (重複確認) $idによるgenreは除く
+      //TRUE  : 重複あり
+      //FALSE : 重複なし
+    public function searchGenre($id, $genre) : bool {
+      $data = [];
+      if($id){
+        $sql = "SELECT * FROM genre_list WHERE is_deleted=0 and id!=? and genre=?";
+        $data[] = $id;
+      }
+      else  $sql = "SELECT * FROM genre_list WHERE is_deleted=0 and genre=?";
+      $data[] = $genre;
+      $stmt = $this->dbh->prepare($sql);
+      $stmt->execute($data);
+
+      $repetition = [];
+      while(TRUE){
+        $rec = [];
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($rec == FALSE) break;
+        $repetition[] = $rec;
+      }
+
+      if($repetition) return TRUE;
+      else  return FALSE;
+    }
+
+
+
     //削除  (is_deleted = 1 とする)
     public function deleteGenre($genre_id) : void {
-      $sql = 'UPDATE genre_list set is_deleted=1 where id=?';
+      $sql = 'UPDATE genre_list SET is_deleted=1 where id=?';
       $stmt = $this->dbh->prepare($sql);
       $data = [];
       $data[] = $genre_id;
       $stmt->execute($data);
     }
-    
+
+
+
+    //編集
+    public function editGenre($id, $name) : void {
+      $sql = 'UPDATE genre_list SET genre=? WHERE id=?';
+      $stmt = $this->dbh->prepare($sql);
+      $data = [];
+      $data[] = $name;
+      $data[] = $id;
+      $stmt->execute($data);
+    }
+
   }
 ?>
