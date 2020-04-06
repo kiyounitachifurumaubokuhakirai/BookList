813261a96c44b11b8fe7dc1ffbc91e72d4084e91
M <>
2020-04-06 13:26:49 +0900

    1. logincheckの返り値をIDに変更 2. 名前を取得する処理を追加 3. 編集する処理を追加 4. 削除する処理を追加

--- htdocs/common/sql_staff.php
+++ htdocs/common/sql_staff.php
@@ -18,10 +18,8 @@
 
 
     //login check
-    public function LoginCheck($staff, $pass) : bool {
-      $flag=FALSE;
-
-      $sql = 'SELECT password FROM staffs_list WHERE username=?';
+    public function LoginCheck($staff, $pass) {
+      $sql = 'SELECT id, password FROM staffs_list WHERE username=?';
       $stmt = $this->dbh->prepare($sql);
       $data = [];
       $data[] = $staff;
@@ -29,9 +27,9 @@
       $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
       //認証処理
-      if(password_verify($pass, $rec['password']))  $flag = TRUE;
+      if(password_verify($pass, $rec['password']))  return $rec['id'];
 
-      return $flag;
+      return 0;
     }
 
 
@@ -50,5 +48,51 @@
     }
 
 
+
+    //staffの名前を取得
+    public function getStaffName($username, $non_hash_pass)  {
+      $sql = "SELECT name, password FROM staffs_list WHERE username=?";
+      $stmt = $this->dbh->prepare($sql);
+      $data = [];
+      $data[] = $username;
+      $stmt->execute($data);
+
+      while(TRUE){
+        $rec = [];
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($rec == FALSE) break;
+        elseif(password_verify($non_hash_pass, $rec['password'])) return $rec['name'];
+      }
+    }
+
+
+
+    //編集
+    public function editStaff($id, $use_name, $new_pass) {
+      $data = [];
+      $data[] = $user_name;
+
+      if($new_pass==""){
+        $data[] = $new_pass;
+        $sql = "UPDATE staffs_list SET username=?, password=? WHERE id=?";
+      }
+      else  $sql = "UPDATE staffs_list SET username=? WHERE id=?";
+
+      $stmt = $this->dbh->prepare($sql);
+      $stmt->execute($data);
+    }
+
+
+
+
+    //削除  (is_deleted = 1 とする)
+    public function deleteStaff($id) : void {
+      $sql = 'UPDATE staffs_list SET is_deleted=1 where id=?';
+      $stmt = $this->dbh->prepare($sql);
+      $data = [];
+      $data[] = $id;
+      $stmt->execute($data);
+    }
   }
+
 ?>
