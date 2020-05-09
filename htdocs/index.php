b196ec78f6896d862b17c6b677c46d68d495f14f
田中哲郎 <sonegaukyokonogorodesu@gmail.com>
2020-05-09 11:06:12 +0900

    $_SESSION["login"]['user']の変数があるかチェックするように修正

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,22 +1,22 @@
 <?php
-  if(!isset($_SESSION)) session_start();
+  if (!isset($_SESSION)) session_start();
   session_regenerate_id(TRUE);
 
   require_once(dirname(__FILE__).'/common/sql_genre.php');
 
-  if(!$_SESSION["login"]['user']){
-    try{
+  if (!isset($_SESSION["login"]['user']) && !$_SESSION["login"]['user'])
+  {
+    try {
       $_SESSION['genre'] = [];
       $genres = new genreModel();
       $_SESSION['genre'] = $genres -> getAllGenre();
-    }
-    catch(Exception $e){
+    } catch(Exception $e)
+    {
       var_dump($e);
       exit();
     }
     $genres= NULL;
   }
-
 
   header('Location: search_books.php');
 ?>
